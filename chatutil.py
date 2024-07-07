f8f6ec60435c4b8a29f34ede396b39c6e0761acc
Chang Sun <goodeffort2003@gmail.com>
2024-07-07 21:30:31 +0800

    add instruction on the model path set up of chattts

--- chatutil.py
+++ chatutil.py
@@ -12,6 +12,7 @@ from modelscope import snapshot_download
 
 module_name = "chatutil"
 
+# do use full path to set the model path. for example "/Users/helloman/assets" . 
 MODELPATH = "~/.cache/huggingface/hub/models--2Noise--ChatTTS/snapshots/cc14302f34d7855eb3420d1fd48345012ff1460d/asset"
  
 class ChatTTSUtil:
